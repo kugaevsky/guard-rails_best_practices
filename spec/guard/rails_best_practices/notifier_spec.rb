081ed0fa66aedbc6e19c5c2abebcac283505e3cd
Nick Kugaevsky <nick@kugaevsky.ru>
2012-07-05 23:02:02 +0400

    Rspec: Notifier tests

--- spec/guard/rails_best_practices/notifier_spec.rb
+++ spec/guard/rails_best_practices/notifier_spec.rb
@@ -3,12 +3,39 @@ require 'spec_helper'
 describe Guard::RailsBestPractices::Notifier do
   subject { Guard::RailsBestPractices::Notifier }
 
-  it 'should call Guard::Notifier' do
+  it 'should format success message' do
+    message = subject.guard_message(true, 5.5)
+    message.should == "Rails Best Practices checklist has been run successfully\nin 5.50 seconds."
+  end
+
+  it 'should format fail message' do
+    message = subject.guard_message(false, 5.5)
+    message.should == "Rails Best Practices checklist run has failed!\nPlease check manually."
+  end
+
+  it 'should select success image' do
+    subject.guard_image(true).should == :success
+  end
+
+  it 'should select failed image' do
+    subject.guard_image(false).should == :failed
+  end
+
+  it 'should call Guard::Notifier for succeed checklist run' do
     ::Guard::Notifier.should_receive(:notify).with(
       "Rails Best Practices checklist has been run successfully\nin 5.50 seconds.",
-      :title => 'Rails Best Practices checklist complete',
+      :title => 'Rails Best Practices',
       :image => :success
     )
     subject.notify(true, 5.5)
+  end
+
+  it 'should call Guard::Notifier for failed checklist run' do
+    ::Guard::Notifier.should_receive(:notify).with(
+      "Rails Best Practices checklist run has failed!\nPlease check manually.",
+      :title => 'Rails Best Practices',
+      :image => :failed
+    )
+    subject.notify(false, 5.5)
   end
 end
